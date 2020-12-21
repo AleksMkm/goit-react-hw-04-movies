c45b129acab55228c633bab9191e0e7ec2061348
Aleksandr Mukomelov <mukomelov.aleksandr@gmail.com>
2020-12-21 18:37:39 +0200

    refactor: Header component reworked with hooks iso class

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,57 +1,53 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import s from './Header.module.css';
+import { usePrevious } from '../../hooks/customHooks';
 
-class Header extends Component {
-  state = {
-    activePage: 'home',
-  };
+function Header({ changeView, currentPage }) {
+  const [activePage, setActivePage] = useState('home');
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.activePage === 'home' &&
-      this.props.currentPage === 'moviecard'
-    ) {
-      this.setState({ activePage: '' });
+  const prevPage = usePrevious(activePage);
+
+  useEffect(() => {
+    if (prevPage === 'home' && currentPage === 'moviecard') {
+      setActivePage('');
     }
-  }
+  }, [currentPage, prevPage]);
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({ [name]: value });
-    this.props.changeView(value);
+  const handleChange = e => {
+    const { value } = e.currentTarget;
+    setActivePage(value);
+    changeView(value);
   };
 
-  render() {
-    return (
-      <header className={s.header}>
-        <div className={s.wrapper}>
-          <label className={s.control}>
-            <input
-              type="radio"
-              className={s.input}
-              value="home"
-              name="activePage"
-              onChange={this.handleChange}
-              checked={this.state.activePage === 'home'}
-            />
-            <span className={s.btn}>Home</span>
-          </label>
-          <label className={s.control}>
-            <input
-              type="radio"
-              className={s.input}
-              value="movies"
-              name="activePage"
-              onChange={this.handleChange}
-              checked={this.state.activePage === 'movies'}
-            />
-            <span className={s.btn}>Movies</span>
-          </label>
-        </div>
-      </header>
-    );
-  }
+  return (
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <label className={s.control}>
+          <input
+            type="radio"
+            className={s.input}
+            value="home"
+            name="activePage"
+            onChange={handleChange}
+            checked={activePage === 'home'}
+          />
+          <span className={s.btn}>Home</span>
+        </label>
+        <label className={s.control}>
+          <input
+            type="radio"
+            className={s.input}
+            value="movies"
+            name="activePage"
+            onChange={handleChange}
+            checked={activePage === 'movies'}
+          />
+          <span className={s.btn}>Movies</span>
+        </label>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
